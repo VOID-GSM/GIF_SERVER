03ec16c74105c73707db8cead9d0c6064e01605b
user <sooooooobinn@gmail.com>
2026-05-06 15:48:46 +0900

    init

---
+++ src/main/java/com/example/gifserverv2/GifServerV2Application.java
@@ -0,0 +1,13 @@
+package com.example.gifserverv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifServerV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GifServerV2Application.class, args);
+    }
+
+}
