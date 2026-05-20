8cbf699af172819b5d59339c5256e82509176e62
user <sooooooobinn@gmail.com>
2026-05-20 13:34:42 +0900

    feat: UserRepository 추가

---
+++ src/main/java/com/example/gifserverv2/domain/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.gifserverv2.domain.user.repository;
+
+import com.example.gifserverv2.domain.user.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByEmail(String email);
+}
