0830f4cb07a3ea4ec8171f0f3a0755dc127a5dc2
user <sooooooobinn@gmail.com>
2026-05-24 02:19:16 +0900

    feat: UpdateScoreRequest 추가

---
+++ src/main/java/com/example/gifserverv2/domain/score/dto/request/UpdateScoreRequest.java
@@ -0,0 +1,10 @@
+package com.example.gifserverv2.domain.score.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UpdateScoreRequest {
+    private Long scoreId;
+}
