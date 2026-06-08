3d474f3034712f60d586e159df3de475b10317b1
hikeong <s25048@gsm.hs.kr>
2026-06-08 16:13:06 +0900

    chore: setter 메서드 추가

--- src/main/java/com/example/gifserverv2/domain/project/dto/request/UpdateProjectRequest.java
+++ src/main/java/com/example/gifserverv2/domain/project/dto/request/UpdateProjectRequest.java
@@ -1,17 +1,18 @@
 package com.example.gifserverv2.domain.project.dto.request;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class UpdateProjectRequest {
     private String name;
     private String teamName;
     private String description;
     private List<Long> addMemberIds;
     private List<Long> removeMemberIds;
-
-    public String getName() { return name; }
-    public String getTeamName() { return teamName; }
-    public String getDescription() { return description; }
-    public List<Long> getAddMemberIds() { return addMemberIds; }
-    public List<Long> getRemoveMemberIds() { return removeMemberIds; }
 }
