d4047de4b3df1efec719c2184125cd302767e65c
hikeong <s25048@gsm.hs.kr>
2026-06-09 13:38:03 +0900

    chore: import문 추가

--- src/main/java/com/example/gifserverv2/domain/project/controller/ProjectController.java
+++ src/main/java/com/example/gifserverv2/domain/project/controller/ProjectController.java
@@ -12,6 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
