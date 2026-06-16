2b9f350387d9314e510370fc99758bb1a3304612
hikeong <s25048@gsm.hs.kr>
2026-06-16 15:15:18 +0900

    chore: 불필요한 공백 삭제

--- src/main/java/com/example/gifserverv2/domain/project/service/QueryProjectService.java
+++ src/main/java/com/example/gifserverv2/domain/project/service/QueryProjectService.java
@@ -22,7 +22,7 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class    QueryProjectService {
+public class QueryProjectService {
 
     private final ProjectRepository projectRepository;
     private final ProjectMemberRepository projectMemberRepository;
