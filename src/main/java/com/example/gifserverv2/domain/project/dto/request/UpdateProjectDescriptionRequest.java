8b5ba63bfaf58a451905cd4a3f4a24616440594f
hikeong <s25048@gsm.hs.kr>
2026-06-23 20:53:11 +0900

    chore: @Size(max = 500)로 유효성 검증

--- src/main/java/com/example/gifserverv2/domain/project/dto/request/UpdateProjectDescriptionRequest.java
+++ src/main/java/com/example/gifserverv2/domain/project/dto/request/UpdateProjectDescriptionRequest.java
@@ -1,5 +1,8 @@
 package com.example.gifserverv2.domain.project.dto.request;
 
+import jakarta.validation.constraints.Size;
+
 public record UpdateProjectDescriptionRequest(
+        @Size(max = 500, message = "프로젝트 설명은 500자 이하여야 합니다.")
         String description
 ) {}
