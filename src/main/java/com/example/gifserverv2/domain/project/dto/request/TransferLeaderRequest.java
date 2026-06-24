9312e1f710ed3ea52e4df3d318711d380f831c98
hikeong <s25048@gsm.hs.kr>
2026-06-24 18:26:06 +0900

    chore: @NotNull 제약조건 추가

--- src/main/java/com/example/gifserverv2/domain/project/dto/request/TransferLeaderRequest.java
+++ src/main/java/com/example/gifserverv2/domain/project/dto/request/TransferLeaderRequest.java
@@ -1,5 +1,8 @@
 package com.example.gifserverv2.domain.project.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 public record TransferLeaderRequest (
+        @NotNull(message = "양도할 팀원 ID는 필수입니다.")
         Long newLeaderUserId
 ){}
