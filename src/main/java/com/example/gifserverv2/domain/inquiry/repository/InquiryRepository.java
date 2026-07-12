fb79526521c6b27f1f1255bf4442fdfef845422a
hikeong <s25048@gsm.hs.kr>
2026-07-12 23:54:33 +0900

    fix: Pageable을 사용하여 페이징 쿼리를 적용

--- src/main/java/com/example/gifserverv2/domain/inquiry/repository/InquiryRepository.java
+++ src/main/java/com/example/gifserverv2/domain/inquiry/repository/InquiryRepository.java
@@ -1,11 +1,12 @@
 package com.example.gifserverv2.domain.inquiry.repository;
 
 import com.example.gifserverv2.domain.inquiry.entity.Inquiry;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
-    List<Inquiry> findAllByCreatedByUserIdOrderByCreatedAtDesc(Long userId);
-    List<Inquiry> findAllByOrderByCreatedAtDesc();
+
+    Page<Inquiry> findAllByCreatedByUserId(Long userId, Pageable pageable);
+    Page<Inquiry> findAll(Pageable pageable);
 }
