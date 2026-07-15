53d6d5ced0c75017a238435926d0dc04d700e987
user <sooooooobinn@gmail.com>
2026-07-15 15:57:26 +0900

    feat: Configuration 설정 추가

---
+++ src/main/java/com/example/gifserverv2/global/config/WebPushConfig.java
@@ -0,0 +1,18 @@
+package com.example.gifserverv2.global.config;
+
+import jakarta.annotation.PostConstruct;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.springframework.context.annotation.Configuration;
+
+import java.security.Security;
+
+@Configuration
+public class WebPushConfig {
+
+    @PostConstruct
+    public void init() {
+        if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null) {
+            Security.addProvider(new BouncyCastleProvider());
+        }
+    }
+}
