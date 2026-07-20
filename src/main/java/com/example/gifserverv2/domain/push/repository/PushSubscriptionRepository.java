d02100a4bc78dc67289c5d24c9fdae6e68e81569
user <sooooooobinn@gmail.com>
2026-07-20 20:11:40 +0900

    feat: 유저 조회 메서드 추가

--- src/main/java/com/example/gifserverv2/domain/push/repository/PushSubscriptionRepository.java
+++ src/main/java/com/example/gifserverv2/domain/push/repository/PushSubscriptionRepository.java
@@ -2,6 +2,8 @@ package com.example.gifserverv2.domain.push.repository;
 
 import com.example.gifserverv2.domain.push.entity.PushSubscription;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface PushSubscriptionRepository extends JpaRepository<PushSubscription, Long> {
@@ -9,4 +11,6 @@ public interface PushSubscriptionRepository extends JpaRepository<PushSubscripti
     Optional<PushSubscription> findByEndpoint(String endpoint);
 
     void deleteByUserIdAndEndpoint(Long userId, String endpoint);
+
+    List<PushSubscription> findAllByUserId(Long userId);
 }
