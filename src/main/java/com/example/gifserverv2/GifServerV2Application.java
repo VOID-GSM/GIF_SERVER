2f5a4f5f1e216d0dde807890cd8dd5ecef9a96bb
user <sooooooobinn@gmail.com>
2026-07-21 17:17:21 +0900

    refactor: EnableAsync 어노테이션 추가

--- src/main/java/com/example/gifserverv2/GifServerV2Application.java
+++ src/main/java/com/example/gifserverv2/GifServerV2Application.java
@@ -3,6 +3,7 @@ package com.example.gifserverv2;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.security.Security;
@@ -10,6 +11,7 @@ import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class GifServerV2Application {
 
     public static void main(String[] args) {
