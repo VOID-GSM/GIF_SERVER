40fbd8afff6d777a1ef6eec374c309ba70aba300
user <sooooooobinn@gmail.com>
2026-07-24 16:38:54 +0900

    feat: Notification entity 추가

---
+++ src/main/java/com/example/gifserverv2/domain/push/entity/Notification.java
@@ -0,0 +1,52 @@
+package com.example.gifserverv2.domain.push.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "notifications")
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, length = 1000)
+    private String body;
+
+    @Column(nullable = false)
+    private boolean isRead;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @Builder
+    public Notification(Long userId, String title, String body) {
+        this.userId = userId;
+        this.title = title;
+        this.body = body;
+        this.isRead = false;
+    }
+
+    public void markAsRead() {
+        this.isRead = true;
+    }
+}
