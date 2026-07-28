3ec8586fb4993b5f14c37cc97a8c805154925e5c
user <sooooooobinn@gmail.com>
2026-07-29 02:18:55 +0900

    refactor: CreatedDate 사용

--- src/main/java/com/example/gifserverv2/domain/push/entity/NotificationHistory.java
+++ src/main/java/com/example/gifserverv2/domain/push/entity/NotificationHistory.java
@@ -5,12 +5,15 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "notifications")
 public class NotificationHistory {
 
@@ -30,13 +33,9 @@ public class NotificationHistory {
     @Column(nullable = false)
     private boolean isRead;
 
+    @CreatedDate
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
-
-    @PrePersist
-    public void prePersist() {
-        this.createdAt = LocalDateTime.now();
-    }
 
     @Builder
     public NotificationHistory(Long userId, String title, String body) {
