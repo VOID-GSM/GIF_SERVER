24cb2d5fdac6cc70467cd9a276325386199a1d73
user <sooooooobinn@gmail.com>
2026-08-19 12:01:14 +0900

    feat: findEvaluatedProjectIdsByEvaluatorId 추가

--- src/main/java/com/example/gifserverv2/domain/score/repository/ScoreRepository.java
+++ src/main/java/com/example/gifserverv2/domain/score/repository/ScoreRepository.java
@@ -20,5 +20,6 @@ public interface ScoreRepository extends JpaRepository<Score, Long> {
     @Query("SELECT s FROM Score s JOIN FETCH s.project")
     List<Score> findAllWithProject();
 
-    boolean existsByEvaluatorId(String evaluatorId);
+    @Query("SELECT s.project.id FROM Score s WHERE s.evaluatorId = :evaluatorId")
+    List<Long> findEvaluatedProjectIdsByEvaluatorId(@Param("evaluatorId") String evaluatorId);
 }
