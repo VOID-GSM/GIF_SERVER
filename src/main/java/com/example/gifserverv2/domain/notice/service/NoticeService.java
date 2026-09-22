ef493765ba8cac4adea23d33255b4bdd019e345e
hikeong <s25048@gsm.hs.kr>
2026-09-22 10:13:20 +0900

    chore: 디스코드 임베드 메세지 문구 수정

--- src/main/java/com/example/gifserverv2/domain/notice/service/NoticeService.java
+++ src/main/java/com/example/gifserverv2/domain/notice/service/NoticeService.java
@@ -106,7 +106,7 @@ public class NoticeService {
                 .setColor(NOTICE_EMBED_COLOR)
                 .setTitle("📢 " + notice.getTitle())
                 .setDescription(notice.getContent())
-                .setFooter("GIF 새로운 공지가 올라왔어요!")
+                .setFooter("GIF 공지")
                 .setTimestamp(notice.getCreatedAt().atZone(ZoneId.systemDefault()));
 
         if (!notice.getTargetGrades().isEmpty()) {
